c29398a30086a08fb79f74d5b35902c462225e29
DELL <selimmurad1111@gmail>
2023-12-16 20:25:44 +0100

    Merge remote-tracking branch 'origin/selim-v1' into selim-v1

--- src/selim/modul19/HotDog.java
+++ src/selim/modul19/HotDog.java
@@ -2,4 +2,21 @@ package selim.modul19;
 
 public class HotDog {
     // Меню
+    public static void main(String[] args) {
+
+    }
+
+    public static class Menu {
+
+    }
+
+    public static class Standart {
+    }
+
+    public static class Max {
+    }
+
+    public static class Mini {
+    }
 }
+
